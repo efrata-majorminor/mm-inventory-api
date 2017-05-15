e66698f8dac7964d48847503112f9c358eb997dd
danielmoonlay <daniel.nababan@moonlay.com>
2017-05-15 09:12:45 +0700

    expedition report

--- server.js
+++ server.js
@@ -43,6 +43,9 @@ inventoryEfrKbRtpModuleRouter.applyRoutes(server, "v1/inventory/docs/efr-kb-rtp"
 var inventoryEfrKbRttReportRouter = require('./src/routers/v1/inventory/inventory-efr-kb-rtt-report-router');
 inventoryEfrKbRttReportRouter.applyRoutes(server, "v1/inventory/docs/efr-kb-rtt/:datefrom/:dateto/:status");
 
+var inventoryEfrKbRttReportRouter = require('./src/routers/v1/inventory/inventory-efr-kb-exp-report-router');
+inventoryEfrKbRttReportRouter.applyRoutes(server, "v1/inventory/docs/efr-kb-exp/report");
+
 var inventoryReceiveModuleRouter = require('./src/routers/v1/inventory/inventory-receive-module-router');
 inventoryReceiveModuleRouter.applyRoutes(server, "v1/inventory/docs");
 
