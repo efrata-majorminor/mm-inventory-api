7102c6b56fb6401ca10860056508ed08edf5a44a
AchmadSetiaji <achmad.setiaji@moonlay.com>
2018-01-16 13:50:08 +0700

    update endpoint
    purchase request by user, post & unpost

---
+++ src/routers/v1/purchasing/purchase-request/purchase-request-unpost-router.js
@@ -0,0 +1,31 @@
+const apiVersion = '1.0.0';
+var Router = require('restify-router').Router;
+var db = require('../../../../db');
+var resultFormatter = require("../../../../result-formatter");
+var Manager = require("bateeq-module").purchasing.PurchasingRequestManager;
+var passport = require('../../../../passports/jwt-passport');
+
+function getRouter() {
+    var router = new Router();
+    router.put('/:id', passport, (request, response, next) => {
+        db.get().then(db => {
+            var manager = new Manager(db, request.user);
+
+            var id = request.params.id;
+
+            manager.unpost(id)
+                .then(docId => {
+                    response.header('Location', `${docId.toString()}`);
+                    var result = resultFormatter.ok(apiVersion, 201);
+                    response.send(201, result);
+                })
+                .catch(e => {
+                    var error = resultFormatter.fail(apiVersion, 400, e);
+                    response.send(400, error);
+                });
+
+        });
+    });
+    return router;
+}
+module.exports = getRouter;
