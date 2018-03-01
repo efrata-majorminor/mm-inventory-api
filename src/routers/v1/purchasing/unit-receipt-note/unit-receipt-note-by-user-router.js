a4626598788965e8c0652ec61cb9c6bba8fba4e8
AchmadSetiaji <achmad.setiaji@moonlay.com>
2018-03-01 14:41:13 +0700

    update: unit-receipt-note-by-user-router.js

--- src/routers/v1/purchasing/unit-receipt-note/unit-receipt-note-by-user-router.js
+++ src/routers/v1/purchasing/unit-receipt-note/unit-receipt-note-by-user-router.js
@@ -1,7 +1,7 @@
 const apiVersion = '1.0.0';
 var Manager = require("bateeq-module").purchasing.UnitReceiptNoteManager;
 var resultFormatter = require("../../../../result-formatter");
-var db = require("../../../db");
+var db = require("../../../../db");
 var JwtRouterFactory = require("../../jwt-router-factory");
 
 var handlePdfRequest = function (request, response, next) {
