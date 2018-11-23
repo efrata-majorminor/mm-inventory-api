a57957cf847dda2754338d89259807e50a0c45b8
AchmadSetiaji <achmad.setiaji@moonlay.com>
2018-11-23 21:46:54 +0700

    update stock opname balance

---
+++ src/routers/v1/inventory/stock-opname-balance-router.js
@@ -0,0 +1,28 @@
+var Router = require('restify-router').Router;;
+var SOBalanceManager = require('bateeq-module').inventory.StockOpnameBalanceManager;
+var db = require('../../../db');
+var resultFormatter = require("../../../result-formatter");
+var passport = require('../../../passports/jwt-passport');
+const apiVersion = '1.0.0';
+
+function getRouter() {
+    var router = new Router();
+    router.get('/storage/:code', passport, (request, response, next) => {
+        db.get().then(db => {
+            var manager = new SOBalanceManager(db, request.user);
+            var code = request.params.code;
+
+            manager.getByStorageCode(code)
+                .then(doc => {
+                    var result = resultFormatter.ok(apiVersion, 200, doc);
+                    response.send(200, result);
+                })
+                .catch(e => {
+                    var error = resultFormatter.fail(apiVersion, 400, e);
+                    response.send(400, error);
+                });
+        });
+    });
+    return router;
+}
+module.exports = getRouter;
